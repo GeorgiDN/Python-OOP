9de1369e090ba872c0040fc8086e06c5f7dcf430
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-09 19:16:41 +0200

    Update 4_cup.py

--- First Steps in OOP -  Exercise/4_cup.py
+++ First Steps in OOP -  Exercise/4_cup.py
@@ -1,15 +1,19 @@
 class Cup:
-    def __init__(self, size: int, quantity: int):
+    def __init__(self, size, quantity):
         self.size = size
         self.quantity = quantity
 
-    def fill(self, quantity):
-        if self.quantity + quantity <= self.size:
-            self.quantity += quantity
+    def get_free_size(self):
+        return self.size - self.quantity
+
+    def fill(self, milliliters):
+        if self.get_free_size() < milliliters:
+            return
+
+        self.quantity += milliliters
 
     def status(self):
-        free_space = self.size - self.quantity
-        return free_space
+        return self.get_free_size()
 
 
 cup = Cup(100, 50)
@@ -17,3 +21,28 @@ print(cup.status())
 cup.fill(40)
 cup.fill(20)
 print(cup.status())
+
+
+
+
+
+
+# class Cup:
+#     def __init__(self, size: int, quantity: int):
+#         self.size = size
+#         self.quantity = quantity
+
+#     def fill(self, quantity):
+#         if self.quantity + quantity <= self.size:
+#             self.quantity += quantity
+
+#     def status(self):
+#         free_space = self.size - self.quantity
+#         return free_space
+
+
+# cup = Cup(100, 50)
+# print(cup.status())
+# cup.fill(40)
+# cup.fill(20)
+# print(cup.status())
