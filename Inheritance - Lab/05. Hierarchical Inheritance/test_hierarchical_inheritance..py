4c65c1967551fb587ac32678d9d700bd3bb5d66c
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-20 21:52:23 +0200

    Create test_hierarchical_inheritance..py

---
+++ Inheritance - Lab/05. Hierarchical Inheritance/test_hierarchical_inheritance..py
@@ -0,0 +1,14 @@
+from project.animal import Animal
+from project.dog import Dog
+from project.cat import Cat
+
+animal = Animal()
+print(animal.eat())
+
+dog = Dog()
+print(dog.bark())
+print(dog.eat())
+
+cat = Cat()
+print(cat.meow())
+print(cat.eat())
