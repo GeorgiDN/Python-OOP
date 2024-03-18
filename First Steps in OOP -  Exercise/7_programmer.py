b1108986c42870d0b45ebd1dcc8ac3a7928a9c47
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-18 21:40:08 +0200

    Update 7_programmer.py

--- First Steps in OOP -  Exercise/7_programmer.py
+++ First Steps in OOP -  Exercise/7_programmer.py
@@ -1,5 +1,5 @@
 class Programmer:
-    def __init__(self, name: str, language: str, skills: int):
+    def __init__(self, name: str, language: str,  skills: int):
         self.name = name
         self.language = language
         self.skills = skills
@@ -8,18 +8,17 @@ class Programmer:
         if self.language == language:
             self.skills += skills_earned
             return f"{self.name} watched {course_name}"
-
         return f"{self.name} does not know {language}"
 
     def change_language(self, new_language, skills_needed):
-        if self.skills >= skills_needed and new_language != self.language:
-            previous_language = self.language
-            self.language = new_language
-            return f"{self.name} switched from {previous_language} to {new_language}"
-        elif self.skills >= skills_needed and new_language == self.language:
+        if self.skills >= skills_needed:
+            if new_language != self.language:
+                previous_language = self.language
+                self.language = new_language
+                return f"{self.name} switched from {previous_language} to {new_language}"
             return f"{self.name} already knows {new_language}"
-        elif self.skills < skills_needed:
-            return f"{self.name} needs {abs(self.skills - skills_needed)} more skills"
+        needed_skills = skills_needed - self.skills
+        return f"{self.name} needs {needed_skills} more skills"
 
 
 programmer = Programmer("John", "Java", 50)
@@ -29,3 +28,32 @@ print(programmer.change_language("Python", 100))
 print(programmer.watch_course("Java: zero to hero", "Java", 50))
 print(programmer.change_language("Python", 100))
 print(programmer.watch_course("Python Masterclass", "Python", 84))
+
+
+
+
+# class Programmer:
+#     def __init__(self, name: str, language: str, skills: int):
+#         self.name = name
+#         self.language = language
+#         self.skills = skills
+
+#     def watch_course(self, course_name, language, skills_earned):
+#         if self.language == language:
+#             self.skills += skills_earned
+#             return f"{self.name} watched {course_name}"
+
+#         return f"{self.name} does not know {language}"
+
+#     def change_language(self, new_language, skills_needed):
+#         if self.skills >= skills_needed and new_language != self.language:
+#             previous_language = self.language
+#             self.language = new_language
+#             return f"{self.name} switched from {previous_language} to {new_language}"
+#         elif self.skills >= skills_needed and new_language == self.language:
+#             return f"{self.name} already knows {new_language}"
+#         elif self.skills < skills_needed:
+#             return f"{self.name} needs {abs(self.skills - skills_needed)} more skills"
+
+
+
