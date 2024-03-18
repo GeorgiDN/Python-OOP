caff2ddbb2ff93468e784632106fafff8d39d63d
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-18 21:11:27 +0200

    Update 4_cup.py

--- First Steps in OOP -  Exercise/4_cup.py
+++ First Steps in OOP -  Exercise/4_cup.py
@@ -1,19 +1,14 @@
 class Cup:
-    def __init__(self, size, quantity):
+    def __init__(self, size: int, quantity: int):
         self.size = size
         self.quantity = quantity
 
-    def get_free_size(self):
-        return self.size - self.quantity
-
-    def fill(self, milliliters):
-        if self.get_free_size() < milliliters:
-            return
-
-        self.quantity += milliliters
+    def fill(self, quantity):
+        if self.quantity + quantity <= self.size:
+            self.quantity += quantity
 
     def status(self):
-        return self.get_free_size()
+        return self.size - self.quantity
 
 
 cup = Cup(100, 50)
@@ -25,24 +20,20 @@ print(cup.status())
 
 
 
-
-
 # class Cup:
-#     def __init__(self, size: int, quantity: int):
+#     def __init__(self, size, quantity):
 #         self.size = size
 #         self.quantity = quantity
 
-#     def fill(self, quantity):
-#         if self.quantity + quantity <= self.size:
-#             self.quantity += quantity
+#     def get_free_size(self):
+#         return self.size - self.quantity
+
+#     def fill(self, milliliters):
+#         if self.get_free_size() < milliliters:
+#             return
+
+#         self.quantity += milliliters
 
 #     def status(self):
-#         free_space = self.size - self.quantity
-#         return free_space
+#         return self.get_free_size()
 
-
-# cup = Cup(100, 50)
-# print(cup.status())
-# cup.fill(40)
-# cup.fill(20)
-# print(cup.status())
