f8e8de298c3c25bda0432dc3e17e0aa38f686c65
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-19 20:30:49 +0200

    Update 2_time.py

--- Classes and Objects - Exercise/2_time.py
+++ Classes and Objects - Exercise/2_time.py
@@ -3,7 +3,7 @@ class Time:
     max_minutes = 59
     max_seconds = 59
 
-    def __init__(self, hours, minutes, seconds):
+    def __init__(self, hours: int, minutes: int, seconds: int):
         self.hours = hours
         self.minutes = minutes
         self.seconds = seconds
@@ -18,16 +18,15 @@ class Time:
 
     def next_second(self):
         self.seconds += 1
-        if self.seconds + 1 > Time.max_seconds:
+        if self.seconds > Time.max_seconds:
             self.seconds = 0
             self.minutes += 1
-        if self.minutes > Time.max_minutes:
-            self.minutes = 0
-            self.hours += 1
-        if self.hours > Time.max_hours:
-            self.hours = 0
-
-        return Time.get_time(self)
+            if self.minutes > Time.max_minutes:
+                self.minutes = 0
+                self.hours += 1
+                if self.hours > Time.max_hours:
+                    self.hours = 0
+        return self.get_time()
 
 
 time = Time(9, 30, 59)
@@ -38,3 +37,37 @@ print(time.next_second())
 
 time = Time(23, 59, 59)
 print(time.next_second())
+
+
+
+
+# from datetime import datetime, timedelta
+# 
+# 
+# class Time:
+#     max_hours = 23
+#     max_minutes = 59
+#     max_seconds = 59
+# 
+#     def __init__(self, hours, minutes, seconds):
+#         self.hours = hours
+#         self.minutes = minutes
+#         self.seconds = seconds
+#         self.time_object = datetime(100, 1, 1, hour=hours, minute=minutes, second=seconds)
+# 
+#     def set_time(self, hours, minutes, seconds):
+#         self.hours = hours
+#         self.minutes = minutes
+#         self.seconds = seconds
+# 
+#     def get_time(self):
+#         # return f"{str(self.hours).zfill(2)}:{str(self.minutes).zfill(2)}:{str(self.seconds).zfill(2)}"
+#         return f"{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
+# 
+#     def next_second(self):
+#         self.time_object = self.time_object + timedelta(seconds=1)
+#         self.hours = self.time_object.hour
+#         self.minutes = self.time_object.minute
+#         self.seconds = self.time_object.second
+# 
+#         return self.get_time()
