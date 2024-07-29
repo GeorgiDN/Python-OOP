2b61c5d570395934d3e9f260527a9b266e72a5ef
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-07-29 10:58:10 +0300

    Update test.py

--- OOP - Exam Preparation/Python OOP Retake Exam - 15 August 2023/03_test/project/test/test.py
+++ OOP - Exam Preparation/Python OOP Retake Exam - 15 August 2023/03_test/project/test/test.py
@@ -4,30 +4,26 @@ from unittest import TestCase, main
 
 class TripTest(TestCase):
     def setUp(self):
-        self.trip = Trip(100000.0, 2, True)
+        self.trip = Trip(20000, 2, True)
 
     def test_init(self):
-        self.assertEqual(100000.0, self.trip.budget)
-        self.assertEqual(2, self.trip.travelers)
+        self.assertIsInstance(self.trip, Trip)
+        self.assertEqual(self.trip.budget, 20000)
+        self.assertEqual(self.trip.travelers, 2)
         self.assertTrue(self.trip.is_family)
-        self.assertEqual({}, self.trip.booked_destinations_paid_amounts)
+        self.assertEqual(self.trip.booked_destinations_paid_amounts, {})
 
-    def test_travelers_with_zero_travelers(self):
-        with self.assertRaises(ValueError) as ve:
-            self.trip.travelers = 0
-        self.assertEqual('At least one traveler is required!', str(ve.exception))
-
-    def test_travelers_with_negative_travelers(self):
+    def test_travelers_with_negative_value_raise_error(self):
         with self.assertRaises(ValueError) as ve:
             self.trip.travelers = -1
-        self.assertEqual('At least one traveler is required!', str(ve.exception))
+        mes = 'At least one traveler is required!'
+        self.assertEqual(str(ve.exception), mes)
 
-    def test_is_family__True_set_to_False(self):
-        self.trip2 = Trip(1000.0, 1, True)
-        self.assertEqual(1000, self.trip2.budget)
-        self.assertEqual(1, self.trip2.travelers)
-        self.assertFalse(self.trip2.is_family)
-        self.assertEqual(self.trip2.booked_destinations_paid_amounts, {})
+    def test_travelers_with_zero_value_raise_error(self):
+        with self.assertRaises(ValueError) as ve:
+            self.trip.travelers = 0
+        mes = 'At least one traveler is required!'
+        self.assertEqual(str(ve.exception), mes)
 
     def test_travelers_with_one_traveler(self):
         self.trip.travelers = 1
@@ -37,84 +33,226 @@ class TripTest(TestCase):
         self.trip.travelers = 2
         self.assertEqual(self.trip.travelers, 2)
 
-    def test_is_family_with_one_traveler_is_family_false(self):
-        self.trip2 = Trip(100000.0, 1, False)
-        result = self.trip2.is_family
-        self.assertFalse(result)
+    def test_is_family_with_one_traveler_and_true_value(self):
+        self.trip = Trip(20000, 1, True)
+        self.assertFalse(self.trip.is_family)
 
-    def test_is_family_with_two_travelers_is_family_true(self):
-        self.trip2 = Trip(100000.0, 2, True)
-        result = self.trip2.is_family
-        self.assertTrue(result)
+    def test_is_family_with_one_traveler_and_false_value(self):
+        self.trip = Trip(20000, 1, False)
+        self.assertFalse(self.trip.is_family)
 
-    #DOES not work with this test
-    # def test_book_a_trip_with_invalid_destination_return_message(self):
-    #     self.trip = Trip(100000.0, 2, True)
-    #     result = self.trip.book_a_trip("Invalid")
-    #     expected_message = 'This destination is not in our offers, please choose a new one!'
-    #     self.assertTrue(result, expected_message)
+    def test_is_family_with_two_travelers_and_false_value(self):
+        self.trip = Trip(20000, 2, False)
+        self.assertFalse(self.trip.is_family)
 
-    def test_book_trip_with_invalid_destination_return_message_is_family_true(self):
-        trip = Trip(1000.0, 2, True)
-        res = trip.book_a_trip("Makedoniq")
-        expected_message = 'This destination is not in our offers, please choose a new one!'
-        self.assertEqual(res, expected_message)
+    def test_is_family_with_three_travelers_and_false_value(self):
+        self.trip = Trip(20000, 3, False)
+        self.assertFalse(self.trip.is_family)
 
-    def test_book_a_trip_with_not_enough_budget_is_family_false(self):
-        self.trip2 = Trip(100.0, 1, False)
-        res = self.trip2.book_a_trip("New Zealand")
-        ex_message = 'Your budget is not enough!'
-        self.assertEqual(res, ex_message)
+    def test_is_family_with_three_travelers_and_true_value(self):
+        self.trip = Trip(20000, 3, True)
+        self.assertTrue(self.trip.is_family)
 
-    def test_book_a_trip_with_not_enough_budget_is_family_true(self):
-        self.trip2 = Trip(100.0, 2, True)
-        res = self.trip2.book_a_trip("New Zealand")
-        ex_message = 'Your budget is not enough!'
-        self.assertEqual(res, ex_message)
+    def test_book_a_trip_with_not_valid_destination(self):
+        res = self.trip.book_a_trip('Invalid')
+        mes = 'This destination is not in our offers, please choose a new one!'
+        self.assertEqual(res, mes)
 
-    def test_book_a_trip_with_enough_budget_is_family_false(self):
-        self.trip2 = Trip(100000.0, 1, False)
-        res = self.trip2.book_a_trip("Bulgaria")
-        ex_message = 'Successfully booked destination Bulgaria! Your budget left is 99500.00'
-        self.assertEqual(res, ex_message)
-        self.assertEqual(99500.0, self.trip2.budget)
-        self.assertEqual(self.trip2.booked_destinations_paid_amounts, {"Bulgaria": 500.0})
+    def test_book_a_trip_with_not_enough_budged_is_family(self):
+        self.trip.budget = 100
+        res = self.trip.book_a_trip('Bulgaria')
+        self.assertEqual(res, 'Your budget is not enough!')
 
-    def test_book_a_trip_with_enough_budget_is_family_true(self):
-        res = self.trip.book_a_trip("Bulgaria")
-        ex_message = 'Successfully booked destination Bulgaria! Your budget left is 99100.00'
-        self.assertEqual(res, ex_message)
-        self.assertEqual(99100.0, self.trip.budget)
-        self.assertEqual(self.trip.booked_destinations_paid_amounts, {"Bulgaria": 900.0})
+    def test_book_a_trip_with_not_enough_budged_is_family_false(self):
+        self.trip.is_family = False
+        self.trip.budget = 100
+        res = self.trip.book_a_trip('Bulgaria')
+        self.assertEqual(res, 'Your budget is not enough!')
 
-    def test_booking_status_with_no_booking_yes(self):
+    def test_book_a_trip_one_traveler_family_false(self):
+        self.trip.is_family = False
+        self.trip.travelers = 1
+        res = self.trip.book_a_trip('Bulgaria')
+        mes = 'Successfully booked destination Bulgaria! Your budget left is 19500.00'
+        self.assertEqual(res, mes)
+        self.assertEqual(self.trip.booked_destinations_paid_amounts, {'Bulgaria': 500.0})
+        self.assertEqual(self.trip.budget, 19500.0)
+
+    def test_book_a_trip_one_traveler_family_true(self):
+        self.trip.is_family = True
+        self.trip.travelers = 1
+        res = self.trip.book_a_trip('Bulgaria')
+        mes = 'Successfully booked destination Bulgaria! Your budget left is 19550.00'
+        self.assertEqual(res, mes)
+        self.assertEqual(self.trip.booked_destinations_paid_amounts, {'Bulgaria': 450.0})
+        self.assertEqual(self.trip.budget, 19550.0)
+
+    def test_book_a_trip_two_travelers_family_false(self):
+        self.trip.is_family = False
+        self.trip.travelers = 2
+        res = self.trip.book_a_trip('Bulgaria')
+        mes = 'Successfully booked destination Bulgaria! Your budget left is 19000.00'
+        self.assertEqual(res, mes)
+        self.assertEqual(self.trip.booked_destinations_paid_amounts, {'Bulgaria': 1000.0})
+        self.assertEqual(self.trip.budget, 19000.0)
+
+    def test_book_a_trip_two_travelers_family_true(self):
+        res = self.trip.book_a_trip('Bulgaria')
+        mes = 'Successfully booked destination Bulgaria! Your budget left is 19100.00'
+        self.assertEqual(res, mes)
+        self.assertEqual(self.trip.booked_destinations_paid_amounts, {'Bulgaria': 900.0})
+        self.assertEqual(self.trip.budget, 19100.0)
+
+    def test_book_a_trip_with_already_added_records(self):
+        self.trip.book_a_trip('New Zealand')
+        res = self.trip.book_a_trip('Bulgaria')
+        mes = 'Successfully booked destination Bulgaria! Your budget left is 5600.00'
+        self.assertEqual(res, mes)
+        self.assertEqual(self.trip.booked_destinations_paid_amounts, {'New Zealand': 13500.0, 'Bulgaria': 900.0})
+        self.assertEqual(self.trip.budget, 5600.0)
+
+    def test_booking_status_with_not_booked_destinations_paid_amounts(self):
         res = self.trip.booking_status()
-        ex_message = f'No bookings yet. Budget: {self.trip.budget:.2f}'
-        self.assertEqual(res, ex_message)
+        mes = f'No bookings yet. Budget: {self.trip.budget:.2f}'
+        self.assertEqual(res, mes)
 
-    def test_booking_status_with_already_made_bookings(self):
-        self.trip2 = Trip(100000.0, 1, False)
-        self.trip2.book_a_trip("Bulgaria")
-        self.trip2.book_a_trip("New Zealand")
-        self.assertEqual(self.trip2.booked_destinations_paid_amounts, {"Bulgaria": 500.0, "New Zealand": 7500.0})
-        result = self.trip2.booking_status()
-        expected_info = "Booked Destination: Bulgaria\n" \
-                        "Paid Amount: 500.00\n" \
-                        "Booked Destination: New Zealand\n" \
-                        "Paid Amount: 7500.00\n" \
-                        "Number of Travelers: 1\n" \
-                        "Budget Left: 92000.00"
-        self.assertEqual(result, expected_info)
+    def test_booking_status_with_booked_destinations_paid_amounts(self):
+        self.trip.book_a_trip('New Zealand')
+        self.trip.book_a_trip('Bulgaria')
+        action = self.trip.booking_status()
+        result = 'Booked Destination: Bulgaria\n'
+        result += 'Paid Amount: 900.00\n'
+        result += 'Booked Destination: New Zealand\n'
+        result += 'Paid Amount: 13500.00\n'
+        result += 'Number of Travelers: 2\n'
+        result += 'Budget Left: 5600.00'
+        self.assertEqual(result, action)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
 
 
 
 
+####################################################################################################################
+# from project.trip import Trip
+# from unittest import TestCase, main
+#
+#
+# class TripTest(TestCase):
+#     def setUp(self):
+#         self.trip = Trip(100000.0, 2, True)
+#
+#     def test_init(self):
+#         self.assertEqual(100000.0, self.trip.budget)
+#         self.assertEqual(2, self.trip.travelers)
+#         self.assertTrue(self.trip.is_family)
+#         self.assertEqual({}, self.trip.booked_destinations_paid_amounts)
+#
+#     def test_travelers_with_zero_travelers(self):
+#         with self.assertRaises(ValueError) as ve:
+#             self.trip.travelers = 0
+#         self.assertEqual('At least one traveler is required!', str(ve.exception))
+#
+#     def test_travelers_with_negative_travelers(self):
+#         with self.assertRaises(ValueError) as ve:
+#             self.trip.travelers = -1
+#         self.assertEqual('At least one traveler is required!', str(ve.exception))
+#
+#     def test_is_family__True_set_to_False(self):
+#         self.trip2 = Trip(1000.0, 1, True)
+#         self.assertEqual(1000, self.trip2.budget)
+#         self.assertEqual(1, self.trip2.travelers)
+#         self.assertFalse(self.trip2.is_family)
+#         self.assertEqual(self.trip2.booked_destinations_paid_amounts, {})
+#
+#     def test_travelers_with_one_traveler(self):
+#         self.trip.travelers = 1
+#         self.assertEqual(self.trip.travelers, 1)
+#
+#     def test_travelers_with_two_travelers(self):
+#         self.trip.travelers = 2
+#         self.assertEqual(self.trip.travelers, 2)
+#
+#     def test_is_family_with_one_traveler_is_family_false(self):
+#         self.trip2 = Trip(100000.0, 1, False)
+#         result = self.trip2.is_family
+#         self.assertFalse(result)
+#
+#     def test_is_family_with_two_travelers_is_family_true(self):
+#         self.trip2 = Trip(100000.0, 2, True)
+#         result = self.trip2.is_family
+#         self.assertTrue(result)
+#
+#     #DOES not work with this test
+#     # def test_book_a_trip_with_invalid_destination_return_message(self):
+#     #     self.trip = Trip(100000.0, 2, True)
+#     #     result = self.trip.book_a_trip("Invalid")
+#     #     expected_message = 'This destination is not in our offers, please choose a new one!'
+#     #     self.assertTrue(result, expected_message)
+#
+#     def test_book_trip_with_invalid_destination_return_message_is_family_true(self):
+#         trip = Trip(1000.0, 2, True)
+#         res = trip.book_a_trip("Makedoniq")
+#         expected_message = 'This destination is not in our offers, please choose a new one!'
+#         self.assertEqual(res, expected_message)
+#
+#     def test_book_a_trip_with_not_enough_budget_is_family_false(self):
+#         self.trip2 = Trip(100.0, 1, False)
+#         res = self.trip2.book_a_trip("New Zealand")
+#         ex_message = 'Your budget is not enough!'
+#         self.assertEqual(res, ex_message)
+#
+#     def test_book_a_trip_with_not_enough_budget_is_family_true(self):
+#         self.trip2 = Trip(100.0, 2, True)
+#         res = self.trip2.book_a_trip("New Zealand")
+#         ex_message = 'Your budget is not enough!'
+#         self.assertEqual(res, ex_message)
+#
+#     def test_book_a_trip_with_enough_budget_is_family_false(self):
+#         self.trip2 = Trip(100000.0, 1, False)
+#         res = self.trip2.book_a_trip("Bulgaria")
+#         ex_message = 'Successfully booked destination Bulgaria! Your budget left is 99500.00'
+#         self.assertEqual(res, ex_message)
+#         self.assertEqual(99500.0, self.trip2.budget)
+#         self.assertEqual(self.trip2.booked_destinations_paid_amounts, {"Bulgaria": 500.0})
+#
+#     def test_book_a_trip_with_enough_budget_is_family_true(self):
+#         res = self.trip.book_a_trip("Bulgaria")
+#         ex_message = 'Successfully booked destination Bulgaria! Your budget left is 99100.00'
+#         self.assertEqual(res, ex_message)
+#         self.assertEqual(99100.0, self.trip.budget)
+#         self.assertEqual(self.trip.booked_destinations_paid_amounts, {"Bulgaria": 900.0})
+#
+#     def test_booking_status_with_no_booking_yes(self):
+#         res = self.trip.booking_status()
+#         ex_message = f'No bookings yet. Budget: {self.trip.budget:.2f}'
+#         self.assertEqual(res, ex_message)
+#
+#     def test_booking_status_with_already_made_bookings(self):
+#         self.trip2 = Trip(100000.0, 1, False)
+#         self.trip2.book_a_trip("Bulgaria")
+#         self.trip2.book_a_trip("New Zealand")
+#         self.assertEqual(self.trip2.booked_destinations_paid_amounts, {"Bulgaria": 500.0, "New Zealand": 7500.0})
+#         result = self.trip2.booking_status()
+#         expected_info = "Booked Destination: Bulgaria\n" \
+#                         "Paid Amount: 500.00\n" \
+#                         "Booked Destination: New Zealand\n" \
+#                         "Paid Amount: 7500.00\n" \
+#                         "Number of Travelers: 1\n" \
+#                         "Budget Left: 92000.00"
+#         self.assertEqual(result, expected_info)
+#
+# if __name__ == "__main__":
+#     main()
 
 
 
+
+
+
+####################################################################################################################
 # from project.trip import Trip
 # from unittest import TestCase, main
 #
