f3a65e23e462b25db2377adecf4ffe22e077716c
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-02-25 07:58:16 +0200

    Update 4_type_check.py

--- Decorators - Exercise/4_type_check.py
+++ Decorators - Exercise/4_type_check.py
@@ -1,10 +1,9 @@
-def type_check(type_):
+def type_check(_type):
     def decorator(func):
-        def wrapper(*args):
-            for el in args:
-                if not isinstance(el, type_):
-                    return "Bad Type"
-            return func(*args)
+        def wrapper(*args, **kwargs):
+            if not isinstance(args[0], _type):
+                return 'Bad Type'
+            return func(*args, **kwargs)
         return wrapper
     return decorator
 
@@ -16,3 +15,25 @@ def times2(num):
 
 print(times2(2))
 print(times2('Not A Number'))
+
+
+
+
+# def type_check(type_):
+#     def decorator(func):
+#         def wrapper(*args):
+#             for el in args:
+#                 if not isinstance(el, type_):
+#                     return "Bad Type"
+#             return func(*args)
+#         return wrapper
+#     return decorator
+
+
+# @type_check(int)
+# def times2(num):
+#     return num*2
+
+
+# print(times2(2))
+# print(times2('Not A Number'))
